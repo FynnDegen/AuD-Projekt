748b56bda871b680710adee79c8ab84b4d7fe0ea
FynnDegen <fynnjoeldegen@gmail.com>
2023-01-18 01:31:50 +0100

    TileMap Sprite hpp

---
+++ Sprite.hpp
@@ -0,0 +1,41 @@
+class Sprite {
+    private:
+        Image pedUp, pedLeft, pedDown, pedRight;
+    public:
+        Sprite() {
+            
+        }
+
+        Sprite(SVG *pSVG, int pX, int pY, std::string pPedUpPath, std::string pPedLeftPath, std::string pPedDownPath, std::string pPedRightPath) {
+            pedUp = Image(pPedUpPath, pX, pY, 100, 100, pSVG);
+            pedLeft = Image(pPedLeftPath, pX, pY, 100, 100, pSVG);;
+            pedDown = Image(pPedDownPath, pX, pY, 100, 100, pSVG);;
+            pedRight = Image(pPedRightPath, pX, pY, 100, 100, pSVG);;
+        }
+    
+        void moveSprite(int pX, int pY) {
+            pedUp.moveTo(pX, pY);
+            pedLeft.moveTo(pX, pY);
+            pedDown.moveTo(pX, pY);
+            pedRight.moveTo(pX, pY);
+        }
+    
+        void toFront(int n) {
+            switch(n) {
+                case 0:
+                    pedUp.toFront();
+                    break;
+                case 1:
+                    pedLeft.toFront();
+                    break;
+                case 2:
+                    pedDown.toFront();
+                    break;
+                case 3:
+                    pedRight.toFront();
+                    break;
+                default:
+                    break;
+            }
+        }
+};
---
+++ TileMap.hpp
@@ -0,0 +1,91 @@
+class TileMap {
+    private:
+        SVG *canvas;
+        Tile tileMap[15][15];
+   
+    public:
+        TileMap() {
+            
+        }
+
+        TileMap(SVG *pCanvas) {
+            canvas = pCanvas;
+            int walls[15][15] = {
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                {1,1,0,0,0,0,0,1,1,1,1,0,0,0,1},
+                {1,0,0,1,1,1,0,0,1,0,0,0,0,0,1},
+                {1,1,0,0,0,0,0,1,1,0,1,0,0,0,1},
+                {1,1,1,1,0,1,0,0,0,0,0,1,0,1,1},
+                {1,1,1,0,0,1,1,1,0,1,0,1,0,1,1},
+                {1,0,0,0,1,0,0,1,0,1,0,0,0,0,1},
+                {1,0,1,0,0,0,1,1,0,1,0,1,1,0,1},
+                {1,0,1,1,1,0,1,1,0,0,0,0,1,0,1},
+                {1,0,0,1,1,0,0,0,0,1,1,0,0,0,1},
+                {1,1,0,0,0,0,1,1,0,1,1,1,1,1,1},
+                {1,1,0,1,1,1,1,1,0,0,0,0,0,1,1},
+                {1,0,0,1,1,0,0,0,0,1,1,0,1,1,1},
+                {1,1,0,0,0,0,1,1,0,1,0,0,0,0,1},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            };
+            
+            for(int i = 0; i < 15; i++) {
+                for(int k = 0; k < 15; k++) {
+                    tileMap[i][k] = Tile(k*100, i*100, k, i, walls[i][k], canvas);
+                }
+            }
+        }
+    
+    int getTilePosX(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY][pMapPosX].getX();
+    }
+    
+    int getTilePosY(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY][pMapPosX].getY();
+    }
+    
+    int getSwordPosX(int pMapPosX, int pMapPosY, int pDirec) {
+        switch(pDirec) {
+            case 0:
+                return tileMap[pMapPosY - 1][pMapPosX].getX() - 50;
+            case 1:
+                return tileMap[pMapPosY][pMapPosX - 1].getX() - 50;
+            case 2:
+                return tileMap[pMapPosY + 1][pMapPosX].getX() - 50;
+            case 3:
+                return tileMap[pMapPosY][pMapPosX + 1].getX() - 50;
+            default:
+                return 0;
+        }
+    }
+    
+    int getSwordPosY(int pMapPosX, int pMapPosY, int pDirec) {
+        switch(pDirec) {
+            case 0:
+                return tileMap[pMapPosY - 1][pMapPosX].getY() - 50;
+            case 1:
+                return tileMap[pMapPosY][pMapPosX - 1].getY() - 50;
+            case 2:
+                return tileMap[pMapPosY + 1][pMapPosX].getY() - 50;
+            case 3:
+                return tileMap[pMapPosY][pMapPosX + 1].getY() - 50;
+            default:
+                return 0;
+        }
+    }
+    
+    int getTileStateUp(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY - 1][pMapPosX].getState();
+    }
+    
+    int getTileStateLeft(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY][pMapPosX - 1].getState();
+    }
+    
+    int getTileStateDown(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY + 1][pMapPosX].getState();
+    }
+    
+    int getTileStateRight(int pMapPosX, int pMapPosY) {
+        return tileMap[pMapPosY][pMapPosX + 1].getState();
+    }
+};
